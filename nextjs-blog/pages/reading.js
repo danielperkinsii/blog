a6733951fb95dd324a2db9b0d5dc541d6b74282b
Daniel Perkins II <daniel.perkins99@gmail.com>
2022-01-21 19:32:24 -0600

    Updated reading list.

--- nextjs-blog/pages/reading.js
+++ nextjs-blog/pages/reading.js
@@ -4,11 +4,16 @@ export default function Reading() {
     return (
         <>
             <Layout reading>
-                <p className="text-red-500">A list of empowering and challenging books, blog posts, and articles.</p>
+                <p className="text-red-500">A list of useful books, blog posts, and articles.</p>
                 <ul>
-                    <li><a href='#'>Book 1</a></li>
-                    <li><a href='#'>Book 2</a></li>
-                    <li><a href='#'>Book 3</a></li>
+                    <li><a href='https://copywritingcourse.com/content-creation-process/'>Copy Writing</a></li>
+                    <li><a href='http://www.paulgraham.com/makersschedule.html'>Maker's Schedule, Manager's Schedule</a></li>
+                    <li><a href='https://www.goodreads.com/book/show/34466963-why-we-sleep'>Why We Sleep</a></li>
+                    <li><a href='https://www.goodreads.com/book/show/8146619-the-right-stuff'>The Right Stuff</a></li>
+                    <li><a href='https://www.goodreads.com/book/show/2195464.What_I_Talk_About_When_I_Talk_About_Running'>What I Talk About When I Talk About Running</a></li>
+                    <li><a href='https://www.goodreads.com/book/show/11275.The_Wind_Up_Bird_Chronicle'>The Wind-Up Bird Chronical</a></li>
+                    <li><a href='https://johnnydecimal.com/'>Johnny Decimal Organization</a></li>
+                    <li><a href='https://www.goodreads.com/book/show/1078.The_Good_Earth'>The Good Earth</a></li>
                 </ul>
             </Layout>
         </>
